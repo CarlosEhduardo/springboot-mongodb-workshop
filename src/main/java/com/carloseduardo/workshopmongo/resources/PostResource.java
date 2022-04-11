8e01afae584c79d0ef8d2cf04b59d96066ce302f
CarlosEhduardo <carlos0eduardo074@gmail.com>
2022-04-11 19:49:06 -0300

    Consulta simples com query methods

--- src/main/java/com/carloseduardo/workshopmongo/resources/PostResource.java
+++ src/main/java/com/carloseduardo/workshopmongo/resources/PostResource.java
@@ -3,13 +3,13 @@ package com.carloseduardo.workshopmongo.resources;
 import com.carloseduardo.workshopmongo.domain.Post;
 import com.carloseduardo.workshopmongo.domain.User;
 import com.carloseduardo.workshopmongo.dto.UserDTO;
+import com.carloseduardo.workshopmongo.resources.util.URL;
 import com.carloseduardo.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -21,6 +21,14 @@ public class PostResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = service.findById(id);
+
+        return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping(value = "titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> post = service.findByTitle(text);
 
         return ResponseEntity.ok().body(post);
     }
--- src/main/java/com/carloseduardo/workshopmongo/services/PostService.java
+++ src/main/java/com/carloseduardo/workshopmongo/services/PostService.java
@@ -7,6 +7,7 @@ import com.carloseduardo.workshopmongo.services.exception.ObjectNotFoundExceptio
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,4 +21,7 @@ public class PostService {
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
+    public List<Post> findByTitle(String text) {
+        return repository.findByTitleContainingIgnoreCase(text);
+    }
 }
