f061dbdbec218201b4ab19190e0bd23cf9fa176f
CarlosEhduardo <carlos0eduardo074@gmail.com>
2022-04-11 18:29:21 -0300

    Entidade Post com user aninhado

---
+++ src/main/java/com/carloseduardo/workshopmongo/repositories/PostRepository.java
@@ -0,0 +1,10 @@
+package com.carloseduardo.workshopmongo.repositories;
+
+import com.carloseduardo.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
